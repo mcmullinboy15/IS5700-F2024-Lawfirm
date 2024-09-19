60911c3b315233bc67aa9a30cc57dd47656973a3
Chance Wiese <chancewiese0925@gmail.com>
2024-09-19 11:46:54 -0600

    chancewiese portfolio and route

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import ChanceWiese from "./portfolios/chancewiese";
 
 function App() {
-  return (
-    <Layout>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </Layout>
-  );
+   return (
+      <Layout>
+         <Router>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="chancewiese" element={<ChanceWiese />} />
+            </Routes>
+         </Router>
+      </Layout>
+   );
 }
 
 export default App;
