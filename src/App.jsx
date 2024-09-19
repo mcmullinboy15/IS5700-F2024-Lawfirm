69ad93d0afda9ab90d1f91fd0c8081616b7ce136
Andrew McMullin <mcmullinboy15@gmail.com>
2024-09-19 12:02:05 -0600

    Added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Mcmullinboy15 from "./portfolios/mcmullinboy15";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/mcmullinboy15" element={<Mcmullinboy15 />} />
         </Routes>
       </Router>
     </Layout>
---
+++ src/portfolios/mcmullinboy15.jsx
@@ -0,0 +1,56 @@
+export default function McMullinBoy15() {
+  return (
+    <>
+      <div className="mt-8 w-full grid grid-cols-3">
+        <div className="col-span-2">
+          <p className="text-center font-bold text-3xl">Andrew McMullin</p>
+          <div className="flex justify-center items-center">
+            <div className="flex gap-x-8">
+              {/* Linked in  */}
+              <a
+                className="text-blue-500"
+                href="https://www.linkedin.com/in/andrew-mcmullin/"
+              >
+                LinkedIn
+              </a>
+              {/* Email */}
+              <a className="text-blue-500" href="mailto:mcmullinand@gmail.com">
+                Email
+              </a>
+              {/* Resume */}
+              <a className="text-blue-500" href="">
+                Resume
+              </a>
+              {/* GitHub */}
+              <a
+                className="text-blue-500"
+                href="https://github.com/McMullinBoy15"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+          <p className="text-center">Software Developer @ OriginMedia</p>
+          <p className="text-center">Greater Salt Lake City Area</p>
+          <p className="text-center w-full mt-8 px-24">
+            Passionate Software Engineer with a love for continuous learning and
+            a deep interest in Automation, Optimizations, Machine Learning, and
+            most of all Software/Full Stack Engineering. From my first semester
+            at the community college, I discovered my passion for software
+            engineering, and since then, I've been dedicated to expanding my
+            knowledge and expertise in all software concepts.
+          </p>
+        </div>
+
+        <div className="col-span-1 mx-auto">
+          <img
+            className="rounded-full"
+            src="https://portfolio.mcmullin.app/assets/profile_pic_face.jpg"
+            height="198"
+            width="265"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
