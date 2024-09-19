2df82d71d6924cc7f04bdbc04096602fe333bcf2
hamnerben <hamnerben@gmail.com>
2024-09-19 11:43:09 -0600

    added hamnerben component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Hamnerben from "./portfolios/hamnerben";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/hamnerben" element={<Hamnerben />} />
         </Routes>
       </Router>
     </Layout>
---
+++ src/portfolios/hamnerben.jsx
@@ -0,0 +1,10 @@
+export default function Hamnerben() {
+    return (
+        <>
+        <div>
+            <h1>Ben Hamner</h1>
+        </div>
+
+        </>
+    );
+}
