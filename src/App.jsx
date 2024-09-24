7053e0a498fc3eba7a1121257faa8d26702ceb8e
Andrew McMullin <mcmullinboy15@gmail.com>
2024-09-24 11:54:21 -0600

    09-24

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,33 @@ import Home from "./pages/Home";
 import ChanceWiese from "./portfolios/chancewiese";
 import Mcmullinboy15 from "./portfolios/mcmullinboy15";
 import AaricP from "./portfolios/aaricp";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    secondary: {
+      light: "#FFD2D2",
+      main: "#FF0000",
+      dark: "#18181B",
+    },
+  },
+});
 
 function App() {
   return (
-    <Layout>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mcmullinboy15" element={<Mcmullinboy15 />} />
-          <Route path="/chancewiese" element={<ChanceWiese />} />
-          <Route path="/aaricp" element={<AaricP />} />
-        </Routes>
-      </Router>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mcmullinboy15" element={<Mcmullinboy15 />} />
+            <Route path="/chancewiese" element={<ChanceWiese />} />
+            <Route path="/aaricp" element={<AaricP />} />
+          </Routes>
+        </Router>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="h-svh bg-background-dark text-white">
       <Header>
         <HeaderButton href="/">Home</HeaderButton>
         <HeaderButton href="/mcmullinboy15">Mcmullinboy15 </HeaderButton>
@@ -15,6 +15,6 @@ export default function Layout({ children }) {
       {children}
 
       <Footer />
-    </>
+    </div>
   );
 }
