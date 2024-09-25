6f34a93b6421c902965377364b827ddfac372c33
hamnerben <hamnerben@gmail.com>
2024-09-24 21:10:02 -0600

    polished portfolio

--- src/portfolios/hamnerben.jsx
+++ src/portfolios/hamnerben.jsx
@@ -18,7 +18,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 800,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -38,7 +38,12 @@ const data ={
             location: 'Lehi, UT',
             date: 'May 2023 - August 2023',
             description: 'Led testing eff orts, ensuring quality and functionality of software products. Developed and executed test plans, collaborating with cross-functional teams. Performed regression, functional, usability, performance, and security testing. Used automated testing to improve effi ciency and streamline repetitive tasks. Contributed to Agile processes, participating in planning and retrospectives. Conducted exploratory testing and validated user stories. Maintained test documentation and reviewed requirements for early defect prevention. Improved QA'},
-    ]
+    ],
+    award: [
+      {name: 'USU Hackathon Finalist - Instagram Client',
+        description: "Recognized as a finalist at the USU Hackathon for leading a dedicated team of four in conceiving and developing an innovative email-based Instagram client within a demanding 24-hour timeframe. Our project stood out for its remarkable utility and practicality, seamlessly integrating email functionality into the Instagram platform . This creation aimed to enhance user experiences by offering a balanced and informed approach to engagement while reducing dependency on constant platform interactions. Our achievement in the USU Hackathon underscored our commitment to creative problem-solving and cutting-edge technology solutions.",
+      }
+    ],
 }
 
   const [open, setOpen] = React.useState(false);
@@ -50,7 +55,8 @@ const data ={
       <>
         <div className="mt-8 w-full grid grid-cols-3">
           <div className="col-span-2">
-            <p className="text-center font-bold text-3xl">Ben Hamner</p>
+            <p className="text-center font-bold text-4xl font-mono">Ben Hamner</p>
+            <p className="text-center text-gray-300 font-light">hamnerben@gmail.com</p>
             <div className="flex justify-center items-center">
               <div className="flex gap-x-8">
                 <a
@@ -71,10 +77,9 @@ const data ={
                 </a>
               </div>
             </div>
-            <p className="text-center">Software Developer at Zendo Health</p>
-            <p className="text-center">Logan</p>
-            <p className="text-center w-full mt-8 px-24">
-              Junior software engineer. USU computer Science student.
+            <p className="text-center">Logan UT</p>
+            <p className="text-center w-full mt-4 px-24">
+              Junior Software Engineer. USU Computer Science senior.
             </p>
           </div>
   
@@ -88,28 +93,29 @@ const data ={
           </div>
         </div>
 
-        <div className='px-8'>
+        <div className="px-8">
             {data.experience.map((exp, index) => (
-      <Accordion>
+      <Accordion className="py-2">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          {exp.jobTitle} at {exp.company}
+          <p className="font-bold text-[20px]">{exp.jobTitle} at {exp.company}</p>
         </AccordionSummary>
         <AccordionDetails>
-            <p>{exp.location}</p>
-            <p>{exp.date}</p>
-          <p>{exp.description}</p>
+            <p className="font-semibold">{exp.location}</p>
+            <p className="font-thin">{exp.date}</p>
+          <p >{exp.description}</p>
         </AccordionDetails>
       </Accordion>
 
             ))}
         </div>
 
+            {data.award.map((award, index) => (
         <div className='px-8'>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen}><p className="text-[18px]">{award.name}</p></Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -117,16 +123,16 @@ const data ={
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography className="text-white " id="modal-modal-title" variant="h6" component="h2">
+          <p className="font-bold text-2xl">{award.name}</p>
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          <Typography className="text-white" id="modal-modal-description" sx={{ mt: 2 }}>
+          {award.description}
           </Typography>
         </Box>
       </Modal>
     </div>
-    
+            ))}
 
       </>
     );
